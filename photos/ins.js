86e7f518923c70920af6986e48a1b78f76dbfa4a
1363653611 <zbcn810@163.com>
2019-12-06 14:23:41 +0800

    Site updated: 2019-12-06 14:23:37

--- photos/ins.js
+++ photos/ins.js
@@ -116,25 +116,24 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = './minins/' + data.link[i] + '.min.jpg';
-          var src = './ins/' + data.link[i];
-          var type = 'images';
-		  var sizes = data.sizes[i];
-		  src += '.jpg';
-          var target = src;
+          var minSrc = 'https://github.com/1363653611/myBlog/tree/develop/blogSrc/blogPhoto/min_photos/' + data.link[i];
+          var src = 'https://github.com/1363653611/myBlog/tree/develop/blogSrc/blogPhoto/photos/' + data.link[i];
+          var type = data.type[i];
+          var target = src + (type === 'video' ? '.mp4' : '.jpg');
+          src += '';
 
-          liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="">\
-                <a href="' + src + '" data-size="' + sizes + '" data-type="' + type + '" data-target="' + target + '">\
-                  <img class="reward-img" data-type="' + type + '" src="' + minSrc + '" itemprop="thumbnail" onload="lzld(this)">\
+          liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
+                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
+                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
         }
-        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '<em>' + data.month + '月</em></h1>\
+        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
         <ul class="img-box-ul">' + liTmpl + '</ul>\
         </section>';
       }
-      document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="">' + ulTmpl + '</div>';
+      document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
       createVideoIncon();
       _view2.default.init();
     };
@@ -170,7 +169,7 @@
     function loadData(success) {
       if (!searchData) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', './ins.json?t=' + +new Date(), true);
+        xhr.open('GET', './data.json?t=' + +new Date(), true);
 
         xhr.onload = function() {
           if (this.status >= 200 && this.status < 300) {
